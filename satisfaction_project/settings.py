c772a44d45785d0b9edf96eb9075fe16fe80e84a
sekougit <sekoudrame339@gmail.com>
2026-04-28 09:28:34 +0000

    update pour forcer la page de connexion au debut

--- satisfaction_project/settings.py
+++ satisfaction_project/settings.py
@@ -132,11 +132,9 @@ PLOTLY_COMPONENTS = [
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-# LOGIN SYSTEM
-LOGIN_URL = 'login'
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
-LOGOUT_REDIRECT_URL = '/'
-
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 # CRISPY FORMS
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
--- satisfaction_project/urls.py
+++ satisfaction_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('login')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('survey/', include('survey.urls')),
